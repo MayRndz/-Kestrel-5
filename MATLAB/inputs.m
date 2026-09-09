802c97fd28224c5535e753976fc3c76dbb14b15a
Gabriella Mayrend <47007278+MayRndz@users.noreply.github.com>
2026-09-09 16:16:06 -0400

    Update initial design assumptions for KESTREL-5 engine

--- MATLAB/inputs.m
+++ MATLAB/inputs.m
@@ -1,18 +1,19 @@
-% Conceptual design parameters
-% All values are initial assumptions that will be changed.
-% as the analysis develops.
+%% KESTREL-5 Conceptual Engine
+% Initial design assumptions
+
+clear;
 
 %% Propellant
 propellant.oxidizer = "LOX";
 propellant.fuel = "RP-1";
 
 %% Engine Design Point
-engine.targetThrust = 5000;          % N
-engine.chamberPressure = 3e6;        % Pa
-engine.mixtureRatio = 2.5;           % O/F
-engine.expansionRatio = 15;          % Ae/At
+engine.targetThrust = 5000;            % N
+engine.chamberPressure = 3.0e6;       % Pa
+engine.mixtureRatio = 2.5;            % O/F
+engine.expansionRatio = 15.0;         % Ae/At
 
 %% Environment
 environment.g0 = 9.80665;             % m/s^2
 environment.seaLevelPressure = 101325; % Pa
-environment.vacuumPressure = 0;       % Pa
+environment.vacuumPressure = 0;        % Pa
