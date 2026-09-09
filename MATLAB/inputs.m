2f771ebf4202e6f9547e7510c63779272712b4e0
Gabriella Mayrend <47007278+MayRndz@users.noreply.github.com>
2026-09-09 16:17:21 -0400

    Added analysis assumptions
    Added analysis assumptions for nozzle and flow conditions.

--- MATLAB/inputs.m
+++ MATLAB/inputs.m
@@ -1,19 +1,25 @@
-%% KESTREL-5 Conceptual Engine
-% Initial design assumptions
-
-clear;
+% Conceptual design parameters
+% All values are initial assumptions that will be changed
+% as the analysis develops.
 
 %% Propellant
 propellant.oxidizer = "LOX";
 propellant.fuel = "RP-1";
 
 %% Engine Design Point
-engine.targetThrust = 5000;            % N
-engine.chamberPressure = 3.0e6;       % Pa
-engine.mixtureRatio = 2.5;            % O/F
-engine.expansionRatio = 15.0;         % Ae/At
+engine.targetThrust = 5000;          % N
+engine.chamberPressure = 3e6;        % Pa
+engine.mixtureRatio = 2.5;           % O/F
+engine.expansionRatio = 15;          % Ae/At
 
 %% Environment
 environment.g0 = 9.80665;             % m/s^2
 environment.seaLevelPressure = 101325; % Pa
-environment.vacuumPressure = 0;        % Pa
+environment.vacuumPressure = 0;       % Pa
+
+%% Analysis Assumptions
+assumptions.isentropicNozzle = true;
+assumptions.steadyFlow = true;
+assumptions.oneDimensionalFlow = true;
+assumptions.uniformChamberPressure = true;
+assumptions.idealGasNozzle = true;
