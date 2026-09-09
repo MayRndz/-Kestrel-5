697a1e84fba58b06a447049b706f22eebae6b1af
Gabriella Mayrend <47007278+MayRndz@users.noreply.github.com>
2026-09-09 16:07:52 -0400

    Add baseline engine inputs

--- MATLAB/inputs.m
+++ MATLAB/inputs.m
@@ -1,4 +1,18 @@
-engine.targetThrust = 5000;
-engine.chamberPressure = 3e6;
-engine.mixtureRatio = 2.5;
-engine.expansionRatio = 15;
+% Conceptual design parameters
+% All values are initial assumptions that will be changed.
+% as the analysis develops.
+
+%% Propellant
+propellant.oxidizer = "LOX";
+propellant.fuel = "RP-1";
+
+%% Engine Design Point
+engine.targetThrust = 5000;          % N
+engine.chamberPressure = 3e6;        % Pa
+engine.mixtureRatio = 2.5;           % O/F
+engine.expansionRatio = 15;          % Ae/At
+
+%% Environment
+environment.g0 = 9.80665;             % m/s^2
+environment.seaLevelPressure = 101325; % Pa
+environment.vacuumPressure = 0;       % Pa
